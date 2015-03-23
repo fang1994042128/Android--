85f0d2040c6fcd1895855aa513322ad50021ade1
fang1994042128 <624941450@qq.com>
2015-03-23 12:45:50 +0800

    股票查询2.0
    增加删除股票以及查看股票详情功能

---
+++ StockerTest/StockerTest/src/com/example/stockertest/DataHandler.java
@@ -0,0 +1,299 @@
+package com.example.stockertest;
+
+import java.io.BufferedInputStream;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DataHandler implements Runnable{
+	int type=0;
+	private static final String TAG="DataHandler";
+	private static final String QUERY_URL="http://hq.sinajs.cn/list=";
+	private static final String SYMBOL_FILE_NAME="symbols.txt";
+	private int BUF_SIZE=16384;
+	private ArrayList<String> stocks=new ArrayList<String>();
+	private ArrayList<StockInfo> stockInfo=new ArrayList<StockInfo>();
+	ArrayList<String> newStocks ;
+	private static final String QUERY_IMG ="http://image.sinajs.cn/newchart/daily/n/";
+	Bitmap bitmap;
+	String symbol;
+	
+	//各个股票信息在数组中的索引
+		private final int NAME=0;
+		private final int OPENING_PRICE=1;
+		private final int CLOSING_PRICE=2;
+		private final int CURRENT_PRICE=3;
+		private final int MAX_PRICE=4;
+		private final int MIN_PRICE=5;
+		Context context;
+		
+		public  DataHandler(Context mContext){
+			context=mContext;
+		}
+		
+		public void addSymbolToFile(String symbol) {
+			for(int i=0;i<stocks.size();i++){
+				if(symbol.equals(stocks.get(i))){
+					return ;
+				}
+			}
+			newStocks = new ArrayList<String>();
+			newStocks.add(symbol);
+			if((newStocks.size()>0) && (newStocks !=null)){
+				type=1;
+				(new Thread(this)).start();
+			}
+			
+		}
+		
+		protected void __addQuotes(ArrayList<String> stockSymbols){
+			if(stockSymbols != null && stockSymbols.size() > 0){
+				int index, count = stockSymbols.size();
+				//取得http客户端实例
+				HttpClient req = new DefaultHttpClient();
+				//用于存放网址
+				StringBuffer buf = new StringBuffer();
+				//构建网址
+				buf.append(QUERY_URL);
+				buf.append(stockSymbols.get(0));
+				//如果股票数超过1
+				for(index = 1; index < count; index++){
+					buf.append(",");	
+					buf.append(stockSymbols.get(index));
+				}
+				try {
+					//采用get方式获取网页数据
+					HttpGet httpGet = new HttpGet(buf.toString());
+					HttpResponse response = req.execute(httpGet);
+					InputStream iStream = response.getEntity().getContent();
+					//解析网页数据，若解析成功则添加股票
+					if(parseQuotesFromStream(iStream))
+					{
+						for(index = 0; index < count; index++){
+							stocks.add(stockSymbols.get(index));
+						}
+						savePortfolio();
+					}
+
+				} catch (IOException e){
+					Log.e(TAG, e.getMessage());
+				}
+			}
+		}
+		
+		public Bitmap getChartForSymbol(String symbol){
+			try {	
+				try {
+					//构建股票K线图网址
+					StringBuilder sb = new StringBuilder(QUERY_IMG);
+					sb = sb.append(symbol+".gif");
+					HttpClient req = new DefaultHttpClient();
+					HttpGet httpGet = new HttpGet(sb.toString());
+					//访问执行网址
+					HttpResponse response = req.execute(httpGet);
+					InputStream iStream;
+					BitmapDrawable bitMap;
+					//取得网址返回的内容
+					iStream = response.getEntity().getContent();
+					//将返回的内容解析成图片
+					bitMap = new BitmapDrawable(iStream);
+					iStream.close();
+					iStream = null;
+					return bitMap.getBitmap();
+				} catch ( IOException iox ){
+					Log.d(TAG, iox.getMessage());	
+				}
+			} catch (Exception e) {
+				Log.d(TAG, e.getMessage());
+			}
+			return null;
+		}
+		
+		private boolean parseQuotesFromStream(InputStream aStream){
+			boolean flag=false;
+			if(aStream != null){
+				//读取数据
+				BufferedInputStream iStream = new BufferedInputStream(aStream);
+				InputStreamReader iReader=null;
+				try {
+					//使用GBK方式解析数据
+					iReader = new InputStreamReader(iStream,"GBK");
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				StringBuffer strBuf = new StringBuffer();
+				char buf[] = new char[BUF_SIZE];
+				try {
+					int charsRead;
+					//将数据读取到StringBuffer中
+					while((charsRead = iReader.read(buf, 0, buf.length)) != -1){
+						strBuf.append(buf, 0, charsRead);
+					}
+					//匹配股票数据
+					Pattern pattern=Pattern.compile("str_(.+)=\"(.+)\"");
+					Matcher matcher=pattern.matcher(strBuf);
+					while(matcher.find()){      
+						//股票信息为第二个括号中对应的内容
+			    		String result=matcher.group(2);
+			    		String[] data=result.split(",");
+			    		StockInfo mStockInfo=new StockInfo();
+			    		//存储股票的代码
+			    		mStockInfo.setNo(matcher.group(1));
+			    		//存储股票名字
+			    		mStockInfo.setName(data[NAME]);
+			    		//存储股票今日开盘价
+			    		mStockInfo.setOpening_price(data[OPENING_PRICE]);
+			    		//存储股票昨日收盘价
+			    		mStockInfo.setClosing_price(data[CLOSING_PRICE]);
+			    		//存储股票当前价格
+			    		mStockInfo.setCurrent_price(Double.parseDouble(data[CURRENT_PRICE])+0.01*(int)(10*Math.random())+"");
+			    		//存储股票今日最高价格
+			    		mStockInfo.setMax_price(data[MAX_PRICE]);
+			    		//存储股票今日最低价格
+			    		mStockInfo.setMin_price(data[MIN_PRICE]);		
+			    		//将数据存储到数组中
+			    		stockInfo.add(mStockInfo);
+			    		flag=true;
+			    	}
+				}catch(IOException iox){
+					Log.e(TAG, iox.getMessage());
+				}
+			}
+			return flag;
+		}
+
+		private void savePortfolio() {
+			if(stocks.size() > 0){
+				FileOutputStream outStream = null;
+				OutputStreamWriter oWriter;
+				try {
+					//打开文件
+					outStream = context.openFileOutput(SYMBOL_FILE_NAME, Context.MODE_PRIVATE);
+					oWriter = new OutputStreamWriter(outStream); 
+					StringBuffer buf = new StringBuffer();
+					int index, count = stocks.size();
+					//构建字符串写入文件中
+					buf.append(stocks.get(0));
+					for(index = 1; index < count; index++){
+						buf.append(",");
+						buf.append(stocks.get(index));
+					}
+					String outStr = buf.toString();
+					oWriter.write(outStr, 0, outStr.length());
+					oWriter.close();
+					outStream.close();
+				} catch(IOException e){
+					Log.e(TAG, e.getMessage());
+				}
+			}
+			
+		}
+		
+		public  StockInfo getQuoteForIndex(int index){
+			return stockInfo.get(index);
+		}
+		
+		public  int stocksSize(){
+			if(stocks != null){
+				return stocks.size();
+			}
+			return 0;
+		}
+		
+		protected ArrayList<StockInfo> getQuotesForArray(ArrayList<String> stockSymbols){
+			if(stockSymbols != null && stockSymbols.size() > 0){
+				//取得http客户端实例
+				HttpClient req = new DefaultHttpClient();
+				//用于存放网址
+				StringBuffer buf = new StringBuffer();
+				int index;
+				//取得股票的总数
+				int count = stockSymbols.size();
+				//构建网址
+				buf.append(QUERY_URL);
+				buf.append(stockSymbols.get(0));
+				//如果股票数超过1
+				for(index = 1; index < count; index++){
+					buf.append(",");	
+					buf.append(stockSymbols.get(index));
+				}
+				try {
+					//采用get方式获取网页数据
+					HttpGet httpGet = new HttpGet(buf.toString());
+					HttpResponse response = req.execute(httpGet);
+					InputStream iStream = response.getEntity().getContent();
+					//解析网页数据
+					stockInfo.clear();
+					parseQuotesFromStream(iStream);
+					//返回股票数据
+					return stockInfo;
+				} catch (IOException e){
+					Log.e(TAG, e.getMessage());
+				}
+				return null;
+			}
+			return null;
+		}
+		
+		public  void refreshStocks(){
+			long startTime = System.currentTimeMillis();
+			long endTime;
+			//取得股票的最新数据
+			type=2;
+			new Thread(this).start();
+			endTime = System.currentTimeMillis();
+			Log.d(TAG, "Refresh ran for " + (endTime - startTime) + " millisenconds");
+		}
+
+		@Override
+		public void run() {
+			if(type==1){
+			__addQuotes(newStocks);
+			type=0;
+			}
+			if(type ==2){
+				getQuotesForArray(stocks);
+				type=0;
+			}
+			if(type ==3){
+				bitmap=getChartForSymbol(symbol);
+			}
+			
+			
+		}
+		
+		public  void removeQuoteByIndex(int index){
+			stocks.remove(index);
+			stockInfo.remove(index);
+			System.out.println("s1"+stocks.size());
+			System.out.println("s2"+stocksSize());
+			System.out.println(stockInfo.size());
+			//保存当前股票
+			savePortfolio();
+		}
+
+		public void  getSymbol(String no) {
+			type=3;
+			symbol=no;
+			new Thread(this).start();
+		}
+		
+
+}
---
+++ StockerTest/StockerTest/src/com/example/stockertest/MainActivity.java
@@ -0,0 +1,353 @@
+package com.example.stockertest;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;
+import android.util.Log;
+import android.view.KeyEvent.Callback;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.Context;
+import android.graphics.Color;
+
+public class MainActivity extends ListActivity implements OnClickListener,Callback {
+	private EditText symbolText;
+	private Button addButton;
+	Context mContext;
+	DataHandler mDataHandler;
+	QuoteAdapter quoteAdapter;
+	
+	private Button cancelButton;
+	//删除按钮
+	private Button deleteButton;
+	//对话框
+	private Dialog dialog = null;
+	//股票详细信息
+	private TextView currentTextView,noTextView, openTextView, closeTextView, dayLowTextView, dayHighTextView;
+	//日K线图
+	private ImageView chartView;
+	int currentSelectedIndex;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext=this;
+        getListView().setEmptyView(findViewById(R.id.empty));
+        File mFile =new File("/data/data/com.example.stockertest/files/symbols.txt");
+		if(mFile.exists())
+		{
+			Log.e("guojs","file exist");
+		}else{
+			try {
+				//新建存放股票代码的文件
+				FileOutputStream outputStream=openFileOutput("symbols.txt",MODE_PRIVATE);
+				outputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			Log.e("guojs","file no exist");
+		}
+		mDataHandler=new DataHandler(mContext);
+        symbolText=(EditText)findViewById(R.id.stock_symbols);
+        addButton=(Button)findViewById(R.id.add_symbols_button);
+        quoteAdapter = new QuoteAdapter(this,mContext,mDataHandler);
+        this.setListAdapter(quoteAdapter);
+        addButton.setOnClickListener(this);
+    }
+    
+    protected void onResume(){
+		super.onResume();
+		if(quoteAdapter != null)
+		{
+			//开始更新界面
+			quoteAdapter.startRefresh();
+		}
+	}
+	//界面不可见时，停止更新
+	protected void onStop(){
+		super.onStop();
+		//停止更新界面
+		quoteAdapter.stopRefresh();
+	}
+    
+    
+
+	@Override
+	public void onClick(View v) {
+		if(v == addButton){
+			addSymbol();
+		}else if(v == cancelButton){
+			//关闭对话框
+			dialog.dismiss();
+		} else if(v== deleteButton){
+			//删除当前股票
+			quoteAdapter.removeQuoteAtIndex(currentSelectedIndex);			
+			dialog.dismiss();
+		} else if(v.getParent() instanceof RelativeLayout){
+			RelativeLayout rl = (RelativeLayout)v.getParent();
+			this.onListItemClick(getListView(), rl, rl.getId()-33, rl.getId());
+		} else if(v instanceof RelativeLayout){
+			this.onListItemClick(getListView(), v, v.getId()-33, v.getId());
+		}
+		
+		
+	}
+	
+	private void addSymbol() {
+		String symbolsStr = symbolText.getText().toString();
+		symbolText.setText("");
+		quoteAdapter.addSymbolsToFile(symbolsStr);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		StockInfo quote = (StockInfo)quoteAdapter.getItem(position);
+		//取得当前位置的序号
+		currentSelectedIndex=position;
+		System.out.println("pso" +position);
+		if(dialog == null){
+			dialog = new Dialog(mContext);
+			dialog.setContentView(R.layout.quote_detail);
+			//删除按钮
+			deleteButton = (Button) dialog.findViewById(R.id.delete);
+			//设置删除按钮监听器
+			deleteButton.setOnClickListener(this);
+			//返回主界面按钮
+			cancelButton = (Button) dialog.findViewById(R.id.close);
+			//设置返回按钮监听器
+			cancelButton.setOnClickListener(this);
+			//当前股票价格
+			currentTextView = (TextView) dialog.findViewById(R.id.current);
+			//当前股票编码
+			noTextView = (TextView) dialog.findViewById(R.id.no);
+			//昨日收盘价
+			openTextView = (TextView) dialog.findViewById(R.id.opening_price);
+			//今日收盘价
+			closeTextView = (TextView) dialog.findViewById(R.id.closing_price);
+			//今日最低价
+			dayLowTextView = (TextView) dialog.findViewById(R.id.day_low);
+			//今日最高价
+			dayHighTextView = (TextView) dialog.findViewById(R.id.day_high);
+			//股票K线图
+			chartView = (ImageView)dialog.findViewById(R.id.chart_view);
+		}
+		//设置对话框标题
+		dialog.setTitle(quote.getName());
+		//设置股票当前价格
+		double current=Double.parseDouble(quote.getCurrent_price());
+		double closing_price=Double.parseDouble(quote.getClosing_price());
+		//保留两位小数
+		DecimalFormat df=new DecimalFormat("#0.00"); 
+		String percent=df.format(((current-closing_price)*100/closing_price))+"%";
+		//若股票价格超过昨日收盘价
+		if(current > closing_price)
+		{
+			//设置字体颜色为红色
+			currentTextView.setTextColor(0xffEE3B3B);			
+		}
+		else 
+		{
+			//设置字体颜色为绿色
+			currentTextView.setTextColor(0xff2e8b57);
+		}
+		mDataHandler.getSymbol(quote.no);
+System.out.println(quote.no);
+		//设置TextView内容
+		currentTextView.setText(df.format(current)+"  ("+percent+")");
+		openTextView.setText(quote.opening_price);
+		closeTextView.setText(quote.closing_price);
+		dayLowTextView.setText(quote.min_price);
+		dayHighTextView.setText(quote.max_price);
+		noTextView.setText(quote.no);
+		//设置K线图
+		chartView.setImageBitmap(mDataHandler.bitmap);
+		dialog.show();
+	}
+
+	public class QuoteAdapter extends BaseAdapter implements  ListAdapter,Runnable{
+		private static final int DISPLAY_COUNT = 10;
+		public DataHandler dataHandler;
+		//强制更新标志
+		private boolean forceUpdate = false;
+		//保存上下文
+		Context context;
+		//保存Activity实例
+		MainActivity stocker;
+		LayoutInflater inflater;
+
+		QuoteRefreshTask quoteRefreshTask = null;
+		int progressInterval;
+		//消息处理器
+		Handler messageHandler = new Handler();
+
+
+		public QuoteAdapter(MainActivity aController, Context mContext,DataHandler mdataHandler) {
+			//保存当前的上下文和Activity实例
+			context = mContext;
+			stocker = aController;
+			dataHandler = mdataHandler;
+		}
+		
+		public void addSymbolsToFile(String symbol){
+			forceUpdate=true;
+			dataHandler.addSymbolToFile(symbol);
+			messageHandler.post(this);
+			Toast.makeText(mContext,"增加股票成功",Toast.LENGTH_LONG).show();
+		}
+		@Override
+		public int getCount() {
+			return dataHandler.stocksSize();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			return dataHandler.getQuoteForIndex(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return arg0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			StockInfo quote;
+			inflater = LayoutInflater.from(context);
+			RelativeLayout cellLayout = (RelativeLayout)inflater.inflate(R.layout.quote_cell, null);
+			cellLayout.setMinimumWidth(parent.getWidth());
+			int color;
+			stocker.setProgress(progressInterval*(position + 1));
+			if(position % 2 > 0)
+				color = Color.rgb(48,92,131);
+			else 
+				color = Color.rgb(119,138,170);
+			cellLayout.setBackgroundColor(color);
+			quote = dataHandler.getQuoteForIndex(position);
+			TextView field = (TextView)cellLayout.findViewById(R.id.symbol);
+			//设置股票的代码
+			field.setText(quote.getNo());
+			field.setClickable(true);
+			field.setOnClickListener(stocker);
+
+			//股票名字
+			field = (TextView)cellLayout.findViewById(R.id.name);
+			field.setClickable(true);
+			field.setOnClickListener(stocker);
+			field.setText(quote.getName());
+			
+			field = (TextView)cellLayout.findViewById(R.id.current);
+			//设置股票当前价格
+			double current=Double.parseDouble(quote.getCurrent_price());
+			double closing_price=Double.parseDouble(quote.getClosing_price());
+			//保留两位小数
+			DecimalFormat df=new DecimalFormat("#0.00"); 
+			String percent=df.format(((current-closing_price)*100/closing_price))+"%";
+			field.setText(df.format(current));
+			field.setClickable(true);
+			field.setOnClickListener(stocker);		
+			
+			field = (TextView)cellLayout.findViewById(R.id.percent);
+			//若股票价格超过昨日收盘价
+			if(current > closing_price)
+			{
+				//设置字体颜色为红色
+				field.setTextColor(0xffEE3B3B);			
+			}
+			else 
+			{
+				//设置字体颜色为绿色
+				field.setTextColor(0xff2e8b57);
+			}
+			field.setText(percent);
+			cellLayout.setId(position + 33);
+			cellLayout.setClickable(true);
+			cellLayout.setOnClickListener(stocker);
+			return cellLayout;
+		}
+
+		@Override
+		public void run() {
+			if(mDataHandler.stocksSize() > 0){
+				if(forceUpdate ){
+					forceUpdate = false;
+					progressInterval = 10000/DISPLAY_COUNT;
+					stocker.setProgressBarVisibility(true);
+					stocker.setProgress(progressInterval);
+				}
+				//通知数据更改
+				this.notifyDataSetChanged();
+			}
+			
+		}
+		public void removeQuoteAtIndex(int index){
+			forceUpdate = true;
+			mDataHandler.removeQuoteByIndex(index);
+			messageHandler.post(this);
+			Toast.makeText(mContext,"删除股票成功",Toast.LENGTH_LONG).show();
+		}
+		public void stopRefresh(){
+			quoteRefreshTask.cancelTimer();
+			quoteRefreshTask = null;
+		}
+		//开始更新股票
+		public void startRefresh(){
+			if(quoteRefreshTask == null)
+				quoteRefreshTask = new QuoteRefreshTask(this);
+		}
+		public class QuoteRefreshTask extends TimerTask {
+			QuoteAdapter quoteAdaptor;
+			Timer        refreshTimer;
+			final static int  TENSECONDS = 10000;
+			public QuoteRefreshTask(QuoteAdapter anAdaptor){
+				refreshTimer = new Timer("Quote Refresh Timer");
+				refreshTimer.schedule(this, TENSECONDS, TENSECONDS);
+				quoteAdaptor = anAdaptor;
+			}
+
+			public void run(){
+				messageHandler.post(quoteAdaptor);
+			}
+
+			public void startTimer(){
+				if(refreshTimer == null){
+					refreshTimer = new Timer("Quote Refresh Timer");
+					refreshTimer.schedule(this, TENSECONDS, TENSECONDS);
+				}
+			}
+			//取消定时器
+			public void cancelTimer(){
+				this.cancel();
+				refreshTimer.cancel();
+				refreshTimer = null;
+			}
+		}
+    	
+    }
+
+    
+}
